89408630628570c1ae9c3aa802b521406d0f9f9e
Ott Toomet <otoomet@gmail.com>
2018-10-13 07:18:30 -0700

    install exercise started

---
+++ install-spark/exercise.R
@@ -0,0 +1,8 @@
+#!/usr/bin/env Rscript
+### This is not really an exercise but a test: can you install spark and get it to run?
+### Try to perform the following steps:
+
+createData <- function(n) {
+   ## create a large file of artificial data and save on disk
+   users <- 
+}
